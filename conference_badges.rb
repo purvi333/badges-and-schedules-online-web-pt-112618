5ac979d5d6fef12ffb21b80386bdb4f9e73af402
benefic-search-4949 <purvivaja14@gmail.com>
2019-01-09 02:14:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def batch_badge_creator(names)
+ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+ x = 0 
+ while x <= speakers.length-1 
+ if names == speakers[x]
+   puts "Hello, my name is #{speakers[x]}."
+ end
+ x +=1
+ end
+ 
+ def assign_rooms(array)
+   array.each do |speaker|
+     batch_badge_creator(speaker)
+     puts "Hello #{speaker}! You will be assigned to room #{array.index(speaker)+1}"
+   end 
+  
+ def printer(array)
+  assign_rooms(array)
+   end
+   
+   
